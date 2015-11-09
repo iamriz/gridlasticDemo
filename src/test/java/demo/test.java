b7cbf9ce6b778f71b88d43f29bf8278a9bbd2ec9
iamriz <iamriz042@gmail.com>
2015-11-09 22:49:25 +0800

    Test8

--- src/test/java/demo/test.java
+++ src/test/java/demo/test.java
@@ -32,11 +32,7 @@ public class test {
 			String browser_version, String hub, ITestContext myTestContext)
 			throws Exception {
 
-<<<<<<< HEAD
 		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-=======
-		DesiredCapabilities capabilities = new DesiredCapabilities();
->>>>>>> f6268c4d883f289f544c5f6c6c8ac95d673c42a6
 //		capabilities.setCapability("jenkins.label", "amd64");
 		capabilities.setPlatform(Platform.LINUX);
 		capabilities.setBrowserName(browser_name); 
